b26c4345935544bb91eae705cd06efd9d55ab0ea
anasmak03 <anas11.makhloufi@gmail.com>
2022-05-28 15:41:32 +0100

    not completed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import logo from "./images/logo.png"
+import logo from "./images/logo.png";
+import Merchant from './Merchant';
+import FAQ from './FAQ';
+import SignUp from './SignUp';
+import Login from './Login';
+import './Header.css'
 export default function Header() {
     const[click,setClick] = useState(false);
     const handleClick = () => setClick(!click)
@@ -16,9 +21,9 @@ export default function Header() {
                     </div>    
                     <ul className={click ? "nav-menu active" : "nav-menu"}>
                         <li className='nav-item'><Link to="/" onClick={handleClick}  className='nav-link Home' >Merchant</Link></li>
-                        <li className='nav-item'><Link to="/intro" onClick={handleClick}  className='nav-link' >FAQ</Link></li>
-                        <li className='nav-item'><Link to="/call" onClick={handleClick}  className='nav-link' href="">Login</Link></li>
-                        <li className='nav-item'><Link to="/projects" onClick={handleClick}  className='nav-link' href=""><Button>Buy electricity Now</Button></Link></li>                    
+                        <li className='nav-item'><Link to="/FAQ" onClick={handleClick}  className='nav-link' >FAQ</Link></li>
+                        <li className='nav-item'><Link to="/SignUp" onClick={handleClick}  className='nav-link' href="">SignUp</Link></li>
+                        <li className='nav-item'><Link to="/Login" onClick={handleClick}  className='nav-link' href="">Login</Link></li>
                     </ul>
 
                     <div onClick={handleClick} className="nav-icon">
@@ -28,10 +33,9 @@ export default function Header() {
                 </nav>  
             </header> 
             <Routes>
-                    <Route path='/' element={<home />}></Route>
-                    <Route path='/Merchant' element={<Merchant />}></Route>
+                    <Route path='/' element={<Merchant />}></Route>
                     <Route path='/FAQ' element={<FAQ />}></Route>
-                    <Route path='/Sign Up' element={<Sign Up />}></Route>
+                    <Route path='/SignUp' element={<SignUp />}></Route>
                     <Route path='/Login' element={<Login />}></Route>
                 </Routes> 
 
---
+++ client/src/components/Merchant.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Merchant extends Component {
+    render() {
+        return (
+            <div>
+               <h1>merchant</h1> 
+            </div>
+        );
+    }
+}
+
+export default Merchant;
